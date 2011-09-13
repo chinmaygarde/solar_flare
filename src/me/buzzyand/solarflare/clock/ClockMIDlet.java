98e73c434e635f05ed06c88a454c60b52716bc45
Chinmay Garde <chinmaygarde@gmail.com>
2011-09-12 19:10:22 -0700

    Removed unused files

--- src/me/buzzyand/solarflare/clock/ClockMIDlet.java
+++ src/me/buzzyand/solarflare/clock/ClockMIDlet.java
@@ -43,7 +43,7 @@ public class ClockMIDlet extends MIDlet implements ISwitchListener {
 		if(switchEvent.getSwitch().hasTag(TAG_CLOCK)) {
 			component = new SolarFlareClock();
 		} else if(switchEvent.getSwitch().hasTag(TAG_TIMER)) {
-			component = new SolarFlareTimer();
+			
 		}
 		
 		if(component != null)
